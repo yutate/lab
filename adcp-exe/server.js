4ce6dcbf5fb909ab2988ee3475f43bf82a3030c0
yutate <yuta.terachi@gmail.com>
2026-05-09 21:35:28 +0900

    fix: explicit CORS config

--- adcp-exe/server.js
+++ adcp-exe/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET","POST"], allowedHeaders: ["Content-Type"] }));
 app.use(express.json());
 
 /* ============================================================
